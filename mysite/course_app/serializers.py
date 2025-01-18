72c423ef4f2673e3dc5b7dfb7118691b5b8c402b
Taalai00 <t.maratbekov07@gmail.com>
2025-01-18 14:42:01 +0600

    serializer, views, urls added

---
+++ mysite/course_app/serializers.py
@@ -0,0 +1,47 @@
+from .models import *
+from rest_framework import serializers
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = '__all__'
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+class AssignmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Assignment
+        fields = '__all__'
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = '__all__'
+
+class ExamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exam
+        fields = '__all__'
+
+class CertificateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Certificate
+        fields = '__all__'
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
---
+++ mysite/course_app/urls.py
@@ -0,0 +1,18 @@
+from .views import *
+from rest_framework import routers
+from django.urls import path, include
+
+router = routers.SimpleRouter()
+router.register(r'user', UserProfileViewSet, basename='user')
+router.register(r'category', CertificateViewSet, basename='category'),
+router.register(r'course', CourseViewSet, basename='course'),
+router.register(r'lesson', LessonViewSet, basename='lesson'),
+router.register(r'assignment', AssignmentViewSet, basename='assignment'),
+router.register(r'question', QuestionViewSet, basename='question'),
+router.register(r'exam', ExamViewSet, basename='exam'),
+router.register(r'certificate', CertificateViewSet, basename='certificate')
+router.register(r'review', ReviewViewSet, basename='review')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
