08d205a7faff76964a5703a2a90b510800f87137
biyke2009 <omurzakovaajbijke27@gmail.com>
2025-01-21 10:18:04 +0600

    admin.py

--- mysite/course_app/admin.py
+++ mysite/course_app/admin.py
@@ -3,12 +3,20 @@ from .models import *
 from modeltranslation.admin import TranslationAdmin
 
 
+@admin.register(Category,Course,Question,Exam,Assignment,Lesson)
+class CategoryAdmin(TranslationAdmin):
+    class Media:
+        js = (
+            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
+            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
+            'modeltranslation/js/tabbed_translation_fields.js',
+        )
+        css = {
+            'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
+        }
+
+
 
 admin.site.register(UserProfile)
-admin.site.register(Course)
-admin.site.register(Lesson)
-admin.site.register(Assignment)
-admin.site.register(Question)
-admin.site.register(Exam)
 admin.site.register(Certificate)
 admin.site.register(Review)
